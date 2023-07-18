d51fd7f853f51b9d132a7d78d12b9b359dc575d6
Elisha <138951172+elishamutang@users.noreply.github.com>
2023-07-19 07:27:41 +0800

    new updates in get_picture

--- shape_calculator.py
+++ shape_calculator.py
@@ -23,7 +23,21 @@ class Rectangle:
         return ((self.width ** 2 + self.height **2) ** .5)
     
     def get_picture(self):
-        pass
+        lines = []
+        for j in range(0, self.height, 1):
+            if j <= self.height+1:
+                line += "\n"
+            else:
+                pass
+
+            for i in range(0, self.width, 1):
+                if i <= self.width+1:
+                    line += "*"
+                else:
+                    line += " "
+        lines.append(line)
+
+        return lines
 
     def get_amount_inside(self, shape):
         pass
